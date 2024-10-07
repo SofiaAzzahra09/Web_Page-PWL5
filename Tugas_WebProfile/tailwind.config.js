a994f39ab4e4bb5aceafe015cd74c91b5907dd94
Sofiaazzahra <sofiaazzahra0001@gmail.com>
2024-10-07 19:48:13 +0700

    merubah isi history dan membuat navbar

--- Tugas_WebProfile/tailwind.config.js
+++ Tugas_WebProfile/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    '../../src/**/*.{html,js}',
+    './page/**/*.html',
+  ],
   theme: {
     extend: {},
   },
